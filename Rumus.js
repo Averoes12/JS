1a04e98b502ca31ec0133e451caa6207b666f5eb
nyusuyo <hz.ir.daff@gmail.com>
2018-09-04 11:27:25 +0700

    math

---
+++ Rumus.js
@@ -0,0 +1,43 @@
+//Rumus persegi
+
+function persegi(a, b){
+    var luas = a * b;
+    
+    return luas;
+
+}
+
+console.log(persegi(5, 6));
+
+//Rumus Segitiga
+function segitiga(a, b, c) {
+    var total= a + b + c;
+    return total;
+}
+
+console.log(segitiga(3, 4, 5));
+
+//rumus kubus
+function kubus(a) {
+    var total = a * a * a;
+    
+    return total;
+}
+console.log(kubus(4));
+
+//rumus tabung
+function tabung(a, b, c) {    
+    var total= a * b * b * c;
+    return total;
+}
+
+console.log(tabung(3.14, 5, 12));
+
+//jajar genjang
+function jajar(a, b) {
+    var total= a * b;
+    
+    return total;
+}
+
+console.log(jajar(32, 89));
