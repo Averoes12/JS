31a05e01402681ae449aa27a3126b534cf970714
nyusuyo <hz.ir.daff@gmail.com>
2018-09-11 15:38:57 +0700

    game-javascript

--- hacktiv8.js
+++ hacktiv8.js
@@ -3,7 +3,11 @@
 var nama = prompt("Pilih nama \n Mikael \n nina \n danu \n zero");
 var peran = prompt("Pilih peran \n Ksatria \n Penyihir \n Tabib");
 
-if (nama === "Miakel" && peran !== peran ) {
+if (nama === "" && peran) {
+	console.log("Masukkan Nama Anda");
+}else if(nama && peran === "") {
+	console.log("Pilih Peran Anda");
+}else if (nama === nama && peran) {
 
     console.log("Halo " + nama + ", Pilih peranmu untuk memulai game!");
 } else if (nama === "nina" && peran === "Ksatria") {
@@ -18,9 +22,6 @@ if (nama === "Miakel" && peran !== peran ) {
 
   console.log("Selamat datang di Dunia Proxytia," + nama);
   console.log("Halo Penyihir" + Zero + ", ciptakan keajaiban yang membantu kemenanganmu!");
-
-} else if(nama !== nama && peran !== peran) {
-  console.log("nama atau peran tidak ada");
 
 } else {
   console.log("nama dan peran harus diisi");
