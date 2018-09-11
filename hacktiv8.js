9cf8de2acda5268ba78ae9393a775ebee29c834a
nyusuyo <hz.ir.daff@gmail.com>
2018-09-11 11:11:19 +0700

    final

--- hacktiv8.js
+++ hacktiv8.js
@@ -5,19 +5,19 @@ var peran = prompt("Pilih peran \n Ksatria \n Penyihir \n Tabib");
 
 if (nama === "Miakel" && peran !== peran ) {
 
-    console.log("Halo Mikael, Pilih peranmu untuk memulai game!");
+    console.log("Halo " + nama + ", Pilih peranmu untuk memulai game!");
 } else if (nama === "nina" && peran === "Ksatria") {
 
-  console.log("Selamat datang di Dunia Proxytia, Nina");
-  console.log("Halo Ksatria Nina, kamu dapat menyerang dengan senjatamu!");
+  console.log("Selamat datang di Dunia Proxytia," + nama);
+  console.log("Halo Ksatria " + nama + ",kamu dapat menyerang dengan senjatamu!");
 } else if (nama === "danu" && peran === "Tabib") {
 
-  console.log("Selamat datang di Dunia Proxytia, Danu");
-  console.log("Halo Tabib Danu, kamu akan membantu temanmu yang terluka.");
+  console.log("Selamat datang di Dunia Proxytia," +nama);
+  console.log("Halo Tabib" + nama + ", kamu akan membantu temanmu yang terluka.");
 } else if (nama === "zero" && peran === "Penyihir") {
 
-  console.log("Selamat datang di Dunia Proxytia, Zero");
-  console.log("Halo Penyihir Zero, ciptakan keajaiban yang membantu kemenanganmu!");
+  console.log("Selamat datang di Dunia Proxytia," + nama);
+  console.log("Halo Penyihir" + Zero + ", ciptakan keajaiban yang membantu kemenanganmu!");
 
 } else if(nama !== nama && peran !== peran) {
   console.log("nama atau peran tidak ada");
