6d1c866e236d5e332757a6f7a8e185f4423e19cb
nyusuyo <hz.ir.daff@gmail.com>
2018-09-14 22:39:03 +0700

    to do counter

--- Tugas-JS/todo-counter.js
+++ Tugas-JS/todo-counter.js
@@ -1,7 +1,4 @@
-var no = parseInt(localStorage.getItem('save'));
-if(isNaN(no)){
-	no = 0;
-}
+var no = localStorage.getItem('save')
 var count = document.getElementById('count');
 var plus = document.getElementById('plus');
 var minus = document.getElementById('minus');
@@ -11,14 +8,12 @@ count.innerHTML = no
 plus.addEventListener('click', function(){
 	no = no + 1  ;
 	console.log("Nomer Ke " + no);
-	count.innerHTML = no
 	localStorage.setItem('save',no);
 })
 
 minus.addEventListener('click', function(){
 	no = no -1 ;
 	console.log("Nomer Ke " + no);
-	count.innerHTML = no
 	localStorage.setItem('save',no);
 
 })
